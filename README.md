38af993e3bc5645199ce5579ddf1dc08dd394b1f
Max W <mwinter@wesleyan.edu>
2021-10-28 15:56:31 -0500

    adding initial Javascript

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Challenge4-Web-API-Quiz
 
 ## Website
+https://mwin1201.github.io/Challenge4-Web-API-Quiz/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-// once user clicks start button the quiz will start
+var startButton = document.querySelector("#start-btn");
+var timer = 60;
+var questionContainerEl = document.querySelector("#question-container");
 
+// once user clicks start button the quiz will start
+startButton.addEventListener("click", function() {
+
+});
 // timer will start running and be displayed to user
+var timerCountdown = setInterval(function() {
+    return timer--;
+}, 1000);
 
 // questions will appear with multiple choices
 
@@ -11,3 +20,14 @@
 // when all the questions are answered OR the timer expires the time remaining becomes the score
 
 // user is presented to save their name/initials to LocalStorage high scores
+
+// array of quiz question objects
+var questionArray = [
+    {
+        question: "What European League does Liverpool play in?",
+        answer1: "1. Serie A",
+        answer2: "2. Bundesliga",
+        answer3: "3. La Liga",
+        answer4: "4. English Premier League"
+    }
+]
