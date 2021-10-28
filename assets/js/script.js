a4d14f1ec34a57164997d34a16684197ce5d6959
Max W <mwinter@wesleyan.edu>
2021-10-28 15:34:33 -0500

    psuedocode added to JS

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// once user clicks start button the quiz will start
+
+// timer will start running and be displayed to user
+
+// questions will appear with multiple choices
+
+// when user selects correct answer, a brief "hooray" message will appear
+
+// when user selects incorrect answer, a brief "not correct" message will appear
+
+// when all the questions are answered OR the timer expires the time remaining becomes the score
+
+// user is presented to save their name/initials to LocalStorage high scores
