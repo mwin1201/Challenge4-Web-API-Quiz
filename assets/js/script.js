314123099f6794c66191864ff56c1c2c57d3d3d2
Max W <mwinter@wesleyan.edu>
2021-10-31 11:16:12 -0500

    adding more logic into quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 var startButton = document.querySelector("#start-btn");
-var timer = 60;
 var questionContainerEl = document.querySelector("#question-container");
 var timerEl = document.querySelector("#timer");
+var headerEl = document.querySelector("#header");
+var timer = 60;
 timerEl.textContent = timer;
 
 // once user clicks start button the quiz will start
 startButton.addEventListener("click", function() {
+    headerEl.textContent = "";
+    startButton.parentElement.removeChild(startButton);
     countdown();
+    displayQuestions();
 });
 
 var countdown = function() {
@@ -14,7 +18,6 @@ var countdown = function() {
     var timerCountdown = setInterval(function() {
         timer--;
         timerEl.textContent = timer;
-        displayQuestions();
 
         if (timer === 0){
             // user is presented to save their name/initials to LocalStorage high scores
@@ -29,25 +32,48 @@ var displayHighScores = function() {
 };
 
 var displayQuestions = function() {
-    var questionEl = document.createElement("div");
-    var listEl = document.createElement("ol");
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
-    for (var i = 0; i < questionArray.length; i++){
+    var i = 0;
+    //while (i < questionArray.length){
+        var questionEl = document.createElement("div");
+        var listEl = document.createElement("ol");
+        var li1 = document.createElement("li");
+        var li2 = document.createElement("li");
+        var li3 = document.createElement("li");
+        var li4 = document.createElement("li");
+        var button1 = document.createElement("button");
+        var button2 = document.createElement("button");
+        var button3 = document.createElement("button");
+        var button4 = document.createElement("button");
+    
         questionEl.textContent = questionArray[i].question;
-        li1.textContent = questionArray[i].answer1;
-        li2.textContent = questionArray[i].answer2;
-        li3.textContent = questionArray[i].answer3;
-        li4.textContent = questionArray[i].answer4;
+        button1.textContent = questionArray[i].answer1;
+        button2.textContent = questionArray[i].answer2;
+        button3.textContent = questionArray[i].answer3;
+        button4.textContent = questionArray[i].answer4;
+    
+        li1.appendChild(button1);
+        li2.appendChild(button2);
+        li3.appendChild(button3);
+        li4.appendChild(button4);
+        listEl.appendChild(li1);
+        listEl.appendChild(li2);
+        listEl.appendChild(li3);
+        listEl.appendChild(li4);
+        questionEl.appendChild(listEl);
+        questionContainerEl.appendChild(questionEl);
+    
+        listEl.addEventListener("click", function(event){
+            checkAnswer(event.target, questionArray[i].correctAnswer);
+            i++;
+        })
+    //}
+};
+
+var checkAnswer = function(userGuess, answer) {
+    userGuess = userGuess.innerHTML;
+    if (userGuess !== answer) {
+        timer = timer - 10;
     }
-    listEl.appendChild(li1);
-    listEl.appendChild(li2);
-    listEl.appendChild(li3);
-    listEl.appendChild(li4);
-    questionEl.appendChild(listEl);
-    questionContainerEl.appendChild(questionEl);
 };
 
 // questions will appear with multiple choices
@@ -62,9 +88,20 @@ var displayQuestions = function() {
 var questionArray = [
     {
         question: "What European League does Liverpool play in?",
-        answer1: "1. Serie A",
-        answer2: "2. Bundesliga",
-        answer3: "3. La Liga",
-        answer4: "4. English Premier League"
+        answer1: "Serie A",
+        answer2: "Bundesliga",
+        answer3: "La Liga",
+        answer4: "English Premier League",
+        correctAnswer: "English Premier League",
+        answerStatus: "unknown"
+    },
+    {
+        question: "Who is the manager of Liverpool right now?",
+        answer1: "Pep Guardiola",
+        answer2: "Ole Gunnar Solskjaer",
+        answer3: "Jurgen Klopp",
+        answer4: "Brendan Rodgers",
+        correctAnswer: "Jurgen Klopp",
+        answerStatus: "unknown"
     }
 ]
