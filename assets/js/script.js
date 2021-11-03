507f9ec678d2dd77d222eca053d77ae7a3faff2c
Max W <mwinter@wesleyan.edu>
2021-11-03 14:21:47 -0500

    refined functions and added more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ var questionContainerEl = document.querySelector("#question-container");
 var timerEl = document.querySelector("#timer");
 var headerEl = document.querySelector("#header");
 bodyEl = document.querySelector("#body");
+var listEl = document.querySelector("#question-answers-list");
+var questionEl = document.querySelector("#question-element");
 var timer = 60;
 timerEl.textContent = timer;
 var questionIndex = 0;
 var highscore = 0;
-
+var highscoresArr = [];
 
 var countdown = function() {
     // timer will start running and be displayed to user
@@ -37,7 +39,13 @@ var displayHighScores = function(highscore) {
     scoreContainer.className = "highscore-container";
     var highscoreList = document.createElement("ol");
     // need to write localstorage values into list
-
+    for (var i = 0; i < highscoresArr.length; i++) {
+        listEl = document.createElement("li");
+        var listObj = loadHighScores(i);
+        listEl.textContent = listObj[i].name + ": " + listObj[i].score;
+        highscoreList.appendChild(listEl);
+        scoreContainer.appendChild(highscoreList);
+    }
     nameLabel = document.createElement("label");
     nameInput = document.createElement("input");
     nameInput.setAttribute("type", "text");
@@ -56,55 +64,57 @@ var displayHighScores = function(highscore) {
 
 };
 
-
-
-var saveScore = function(playerObj) {
-    localStorage.setItem("players", JSON.stringify(playerObj));
+var loadHighScores = function(index) {
+    var scores = localStorage.getItem("Quiz-Game");
+    if (!scores) {
+        return false;
+    }
+    scores = JSON.parse(scores);
+    return scores[index];
 };
 
+var saveScore = function(playerObj) {
+    highscoresArr.push(playerObj);
+    var scores = localStorage.getItem("Quiz-Game");
+    scores = JSON.parse(scores);
+    highscoresArr.push(scores);
+    localStorage.setItem("Quiz-Game", JSON.stringify(highscoresArr));
+};
+
+// function to display the quiz questions for the user
 var displayQuestions = function(quizQuestion) {
-    var questionEl = document.createElement("div");
-    var listEl = document.createElement("ol");
+    // create list items
     var li1 = document.createElement("li");
     var li2 = document.createElement("li");
     var li3 = document.createElement("li");
     var li4 = document.createElement("li");
+    // create buttons for each list item multiple choice answer
     var button1 = document.createElement("button");
     var button2 = document.createElement("button");
     var button3 = document.createElement("button");
     var button4 = document.createElement("button");
 
+    // populate the new elements with text for the questions and multiple choice answers
     questionEl.textContent = quizQuestion.question;
     button1.textContent = quizQuestion.answer1;
     button2.textContent = quizQuestion.answer2;
     button3.textContent = quizQuestion.answer3;
     button4.textContent = quizQuestion.answer4;
 
+    // add the buttons to each list item
     li1.appendChild(button1);
     li2.appendChild(button2);
     li3.appendChild(button3);
     li4.appendChild(button4);
+    // add each list item to the lits element
     listEl.appendChild(li1);
     listEl.appendChild(li2);
     listEl.appendChild(li3);
     listEl.appendChild(li4);
+    // add the entire list to the question div element
     questionEl.appendChild(listEl);
+    // add the question element to the question container
     questionContainerEl.appendChild(questionEl);
-
-    listEl.addEventListener("click", function(event){
-        checkAnswer(event.target, quizQuestion.correctAnswer);
-        while (questionIndex < questionArray.length) {
-            questionEl.textContent= "";
-            questionIndex++;
-            if (questionIndex > questionArray.length - 1){
-                break;
-            }
-            return displayQuestions(questionArray[questionIndex]);
-        }
-        highscore = timer;
-        displayHighScores(highscore);
-        timer = 0;
-    });
 };
 
 
@@ -121,6 +131,13 @@ var checkAnswer = function(userGuess, answer) {
     }
 };
 
+// remove question and answers
+var removeQuestion = function() {
+    while (listEl.hasChildNodes()) {
+        listEl.removeChild(listEl.firstChild);
+    }
+    questionEl.removeChild(listEl);
+};
 // questions will appear with multiple choices
 
 // when user selects correct answer, a brief "hooray" message will appear
@@ -148,6 +165,33 @@ var questionArray = [
         answer4: "Brendan Rodgers",
         correctAnswer: "Jurgen Klopp",
         answerStatus: "unknown"
+    },
+    {
+        question: "Which Liverpool player is from Egypt?",
+        answer1: "Virgil van Dijk",
+        answer2: "Sadio Mane",
+        answer3: "Mohamed Salah",
+        answer4: "Divock Origi",
+        correctAnswer: "Mohamed Salah",
+        answerStatus: "unknown"
+    },
+    {
+        question: "Which Liverpool player scored their 100th Premier League goal this season?",
+        answer1: "Sadio Mane",
+        answer2: "Mohamed Salah",
+        answer3: "Christiano Ronaldo",
+        answer4: "Lionel Messi",
+        correctAnswer: "Sadio Mane",
+        answerStatus: "unknown"
+    },
+    {
+        question: "Liverpool defeated which team 4-0 in 2019 to reach the Champions League finals?",
+        answer1: "Manchester City",
+        answer2: "Barcelona",
+        answer3: "Bayern Munich",
+        answer4: "Manchester United",
+        correctAnswer: "Barcelona",
+        answerStatus: "unknown"
     }
 ]
 
@@ -158,4 +202,19 @@ startButton.addEventListener("click", function() {
     startButton.parentElement.removeChild(startButton);
     countdown();
     displayQuestions(questionArray[questionIndex]);
+});
+
+listEl.addEventListener("click", function(event){
+    checkAnswer(event.target, questionArray[questionIndex].correctAnswer);
+    while (questionIndex < questionArray.length) {
+        removeQuestion();
+        questionIndex++;
+        if (questionIndex > questionArray.length - 1){
+            break;
+        }
+        return displayQuestions(questionArray[questionIndex]);
+    }
+    highscore = timer;
+    displayHighScores(highscore);
+    timer = 0;
 });
